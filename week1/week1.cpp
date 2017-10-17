a11450e1bdf6cb13dd883b6deac6b269b81ab6d4
Joan Karadimov <joan.karadimov@gmail.com>
2017-10-17 19:44:08 +0300

    Add materials for week 1 of the course

---
+++ week1/week1.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <math.h>
+#include <string>
+
+#include "../vecta/vecta.h"
+
+template<typename T> void print_with_type(T x) {
+	std::cout << typeid(x).name() << ": " << x << std::endl;
+}
+
+template <typename N>
+std::ostream& operator<< (std::ostream& out, const vecta::vec2d<N>& v) {
+	return out << '(' << v.x << ", " << v.y << ')';
+}
+
+int main()
+{
+	vecta::vec2d<> a(2, 0);
+	vecta::vec2d<> b(9, 0);
+	vecta::vec2d<> l(0, 8);
+	vecta::vec2d<> r(10, 6);
+	vecta::vec2d<> q(3, 10);
+
+	auto length_ab = vecta::len(-a + b);
+	auto length_ab_side = abs((-l + r) * (-a + b)) / length_ab;
+	auto length_ab_normal_side = abs((-a + b) ^ (-a + q)) / length_ab;
+
+	/*print_with_type(length_ab);
+	print_with_type(length_ab_side);
+	print_with_type(length_ab_normal_side);*/
+
+	//std::stringstream sp_input;
+	//sp_intput << "echo -e sp svg " <<
+	system("printf 'points\\n1 1\\n2 2\\n' | C:\\dev\\work\\computational-graphics\\x64\\Debug\\sp svg > asd.svg");
+	//system("\"\"C:\\Program Files(x86)\\Google\\Chrome\\Application\\chrome.exe\" asd.svg\"");
+	//system("\"\"c:\\program files\\internet explorer\\iexplore.exe\" \"asd.svg\"\"");
+	std::cout << length_ab_side * length_ab_normal_side << std::endl;
+
+    return 0;
+}
